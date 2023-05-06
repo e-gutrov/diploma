d759bd25d0a44e7cc2fde7b036e420b7be09ec8b
Egor Gutrov <gutrov.egor@gmail.com>
2023-05-06 19:03:22 +0100

    Added code for LLVM YSON validation

--- yson_validators_llvm.cpp
+++ yson_validators_llvm.cpp
@@ -1,5 +1,53 @@
-//
-// Created by egor on 06/05/23.
-//
+#include <ytsaurus/contrib/libs/llvm16/include/llvm/IR/IRBuilder.h>
+#include <ytsaurus/contrib/libs/llvm16/include/llvm/IRReader/IRReader.h>
+#include <ytsaurus/contrib/libs/llvm16/include/llvm/Support/SourceMgr.h>
 
 #include "yson_validators_llvm.h"
+#include "helpers.h"
+
+namespace YsonValidators {
+    NYT::NYson::TYsonPullParserCursor* toYsonCursor(void* ptr) {
+        return reinterpret_cast<NYT::NYson::TYsonPullParserCursor*>(ptr);
+    }
+
+    bool IsDone(void* ptr) {
+        auto cursor = toYsonCursor(ptr);
+        return cursor->GetCurrent().IsEndOfStream();
+    }
+
+    NYT::NYson::EYsonItemType GetCurrentType(void* ptr) {
+        auto cursor = toYsonCursor(ptr);
+        return cursor->GetCurrent().GetType();
+    }
+
+    void CallNext(void* ptr) {
+        auto cursor = toYsonCursor(ptr);
+        cursor->Next();
+    }
+
+    llvm::orc::ThreadSafeModule CreateTableSchemaValidator(const TypeBasePtr& schema) {
+        auto context = std::make_unique<llvm::LLVMContext>();
+        llvm::IRBuilder<> builder(*context);
+        llvm::SMDiagnostic err;
+        auto module = llvm::parseIRFile("/home/egor/CLionProjects/coursework/llvm-ir-helpers/llvm_yson_helpers.ll", err, *context);
+        auto functions = GenerateFunctionDeclarations(&builder, module.get());
+
+        auto validateRoot = CreateTypeValidator(schema, context.get(), &builder, module.get(), functions);
+
+        auto mainType = llvm::FunctionType::get(builder.getInt1Ty(), {builder.getInt8PtrTy()}, false);
+        auto mainFunc = llvm::Function::Create(mainType, llvm::Function::ExternalLinkage, "main", module.get());
+
+        auto entry = llvm::BasicBlock::Create(*context, "entry", mainFunc);
+        builder.SetInsertPoint(entry);
+
+        auto callValidateRoot = builder.CreateCall(validateRoot, {mainFunc->arg_begin()});
+        llvm::Value* validateResult = callValidateRoot;
+        auto callIsDone = builder.CreateCall(functions.at("IsDone"), {mainFunc->arg_begin()});
+        llvm::Value* isDone = callIsDone;
+
+        auto result = builder.CreateAnd(validateResult, isDone);
+        builder.CreateRet(result);
+
+        return FinalizeModule(std::move(module), std::move(context));
+    }
+}
--- yson_validators_llvm.h
+++ yson_validators_llvm.h
@@ -1,8 +1,33 @@
-//
-// Created by egor on 06/05/23.
-//
-
 #ifndef COURSEWORK_YSON_VALIDATORS_LLVM_H
 #define COURSEWORK_YSON_VALIDATORS_LLVM_H
+
+#include <ytsaurus/contrib/libs/llvm16/include/llvm/ExecutionEngine/Orc/ThreadSafeModule.h>
+#include <ytsaurus/yt/yt/core/yson/pull_parser.h>
+
+#include "table_schema.h"
+
+namespace YsonValidators {
+    NYT::NYson::EYsonItemType GetCurrentType(void* ptr);
+    bool IsDone(void* ptr);
+    void CallNext(void* ptr);
+
+    template<ValueType T>
+    bool ValidateSimpleType(void* ptr) {
+        auto eventType = GetCurrentType(ptr);
+        CallNext(ptr);
+        if constexpr (T == ValueType::Int) {
+            return eventType == NYT::NYson::EYsonItemType::Uint64Value || eventType == NYT::NYson::EYsonItemType::Int64Value;
+        } else if constexpr (T == ValueType::String) {
+            return eventType == NYT::NYson::EYsonItemType::StringValue;
+        }
+    }
+
+    template<NYT::NYson::EYsonItemType T>
+    bool IsType(void* ptr) {
+        return GetCurrentType(ptr) == T;
+    }
+
+    llvm::orc::ThreadSafeModule CreateTableSchemaValidator(const TypeBasePtr& schema);
+}
 
 #endif //COURSEWORK_YSON_VALIDATORS_LLVM_H
